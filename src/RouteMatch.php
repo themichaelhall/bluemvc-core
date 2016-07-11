574a7c6c933b229d952390d3eecb227ffd8b9f44
Michael Hall <hall@michaelhall.se>
2016-07-11 22:18:58 +0200

    Create RouteMatch class.

---
+++ src/RouteMatch.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace BlueMvc\Core;
+
+use BlueMvc\Core\Interfaces\RouteMatchInterface;
+
+/**
+ * Class representing a route match.
+ */
+class RouteMatch implements RouteMatchInterface
+{
+}
