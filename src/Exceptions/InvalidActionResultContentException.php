e3e173da7e357b05f509f621c8327a001c40ceb9
Michael Hall <hall@michaelhall.se>
2017-07-05 20:21:21 +0200

    Add InvalidActionResultContentException class

---
+++ src/Exceptions/InvalidActionResultContentException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is a part of the bluemvc-core package.
+ *
+ * Read more at https://bluemvc.com/
+ */
+
+namespace BlueMvc\Core\Exceptions;
+
+/**
+ * Exception used when an invalid content is used to create an action result.
+ *
+ * @since 1.0.0
+ */
+class InvalidActionResultContentException extends \Exception
+{
+}
