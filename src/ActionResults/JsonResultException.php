20662a2c9afbdd46f284055807662b1b7c4e8fd7
Michael Hall <hall@michaelhall.se>
2018-06-26 20:47:05 +0200

    Add JsonResultException class

---
+++ src/ActionResults/JsonResultException.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is a part of the bluemvc-core package.
+ *
+ * Read more at https://bluemvc.com/
+ */
+declare(strict_types=1);
+
+namespace BlueMvc\Core\ActionResults;
+
+use BlueMvc\Core\Exceptions\InvalidActionResultContentException;
+use BlueMvc\Core\Interfaces\Http\StatusCodeInterface;
+
+/**
+ * Class representing a JSON action result exception.
+ *
+ * @since 2.1.0
+ */
+class JsonResultException extends ActionResultException
+{
+    /**
+     * Constructs the action result exception.
+     *
+     * @since 2.1.0
+     *
+     * @param mixed                    $content    The content.
+     * @param StatusCodeInterface|null $statusCode The status code or null for status code 200 OK.
+     *
+     * @throws InvalidActionResultContentException If the content could not be json-encoded.
+     */
+    public function __construct($content, ?StatusCodeInterface $statusCode = null)
+    {
+        parent::__construct(new JsonResult($content, $statusCode));
+    }
+}
---
+++ tests/ActionResults/JsonResultExceptionTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BlueMvc\Core\Tests\ActionResults;
+
+use BlueMvc\Core\ActionResults\JsonResultException;
+use BlueMvc\Core\Http\Method;
+use BlueMvc\Core\Http\StatusCode;
+use BlueMvc\Core\Tests\Helpers\TestApplications\BasicTestApplication;
+use BlueMvc\Core\Tests\Helpers\TestClasses\JsonSerializableTestClass;
+use BlueMvc\Core\Tests\Helpers\TestRequests\BasicTestRequest;
+use BlueMvc\Core\Tests\Helpers\TestResponses\BasicTestResponse;
+use DataTypes\FilePath;
+use DataTypes\Url;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test JsonResultException class.
+ */
+class JsonResultExceptionTest extends TestCase
+{
+    /**
+     * Test with content.
+     */
+    public function testWithContent()
+    {
+        $application = new BasicTestApplication(FilePath::parse('/var/www/'));
+        $request = new BasicTestRequest(Url::parse('https://www.example.com/foo/bar'), new Method('GET'));
+        $response = new BasicTestResponse();
+        $actionResultException = new JsonResultException(['Foo' => 'Bar']);
+        $actionResultException->getActionResult()->updateResponse($application, $request, $response);
+
+        self::assertSame(200, $response->getStatusCode()->getCode());
+        self::assertSame('OK', $response->getStatusCode()->getDescription());
+        self::assertSame('{"Foo":"Bar"}', $response->getContent());
+        self::assertSame('application/json', $response->getHeader('Content-Type'));
+    }
+
+    /**
+     * Test with status code.
+     */
+    public function testWithStatusCode()
+    {
+        $application = new BasicTestApplication(FilePath::parse('/var/www/'));
+        $request = new BasicTestRequest(Url::parse('https://www.example.com/foo/bar'), new Method('GET'));
+        $response = new BasicTestResponse();
+        $actionResultException = new JsonResultException(['Error' => 'Not Found'], new StatusCode(StatusCode::NOT_FOUND));
+        $actionResultException->getActionResult()->updateResponse($application, $request, $response);
+
+        self::assertSame(404, $response->getStatusCode()->getCode());
+        self::assertSame('Not Found', $response->getStatusCode()->getDescription());
+        self::assertSame('{"Error":"Not Found"}', $response->getContent());
+        self::assertSame('application/json', $response->getHeader('Content-Type'));
+    }
+
+    /**
+     * Test with JsonSerializable class.
+     */
+    public function testWithJsonSerializable()
+    {
+        $application = new BasicTestApplication(FilePath::parse('/var/www/'));
+        $request = new BasicTestRequest(Url::parse('https://www.example.com/foo/bar'), new Method('GET'));
+        $response = new BasicTestResponse();
+        $actionResultException = new JsonResultException(new JsonSerializableTestClass(10, 'Foo'));
+        $actionResultException->getActionResult()->updateResponse($application, $request, $response);
+
+        self::assertSame(200, $response->getStatusCode()->getCode());
+        self::assertSame('OK', $response->getStatusCode()->getDescription());
+        self::assertSame('{"text":"Foo"}', $response->getContent());
+        self::assertSame('application/json', $response->getHeader('Content-Type'));
+    }
+
+    /**
+     * Test with invalid json data class.
+     *
+     * @expectedException \BlueMvc\Core\Exceptions\InvalidActionResultContentException
+     * @expectedExceptionMessage Could not create JsonResult from content: Malformed UTF-8 characters, possibly incorrectly encoded
+     */
+    public function testWithInvalidJsonData()
+    {
+        new JsonResultException(chr(255));
+    }
+}
