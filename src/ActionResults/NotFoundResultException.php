2b49518b0c3e395ed833aba478492fff374990da
Michael Hall <hall@michaelhall.se>
2018-06-27 19:08:32 +0200

    Add NotFoundResultException class

---
+++ src/ActionResults/NotFoundResultException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is a part of the bluemvc-core package.
+ *
+ * Read more at https://bluemvc.com/
+ */
+declare(strict_types=1);
+
+namespace BlueMvc\Core\ActionResults;
+
+/**
+ * Class representing a 404 Not Found action result exception.
+ *
+ * @since 2.1.0
+ */
+class NotFoundResultException extends ActionResultException
+{
+    /**
+     * Constructs the action result exception.
+     *
+     * @since 2.1.0
+     *
+     * @param string $content The content.
+     */
+    public function __construct(string $content = '')
+    {
+        parent::__construct(new NotFoundResult($content));
+    }
+}
---
+++ tests/ActionResults/NotFoundResultExceptionTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BlueMvc\Core\Tests\ActionResults;
+
+use BlueMvc\Core\ActionResults\NotFoundResultException;
+use BlueMvc\Core\Http\Method;
+use BlueMvc\Core\Tests\Helpers\TestApplications\BasicTestApplication;
+use BlueMvc\Core\Tests\Helpers\TestRequests\BasicTestRequest;
+use BlueMvc\Core\Tests\Helpers\TestResponses\BasicTestResponse;
+use DataTypes\FilePath;
+use DataTypes\Url;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test NotFoundResultException class.
+ */
+class NotFoundResultExceptionTest extends TestCase
+{
+    /**
+     * Test default constructor.
+     */
+    public function testDefaultConstructor()
+    {
+        $application = new BasicTestApplication(FilePath::parse('/var/www/'));
+        $request = new BasicTestRequest(Url::parse('https://www.example.com/foo/bar'), new Method('GET'));
+        $response = new BasicTestResponse();
+        $actionResultException = new NotFoundResultException();
+        $actionResultException->getActionResult()->updateResponse($application, $request, $response);
+
+        self::assertSame(404, $response->getStatusCode()->getCode());
+        self::assertSame('Not Found', $response->getStatusCode()->getDescription());
+        self::assertSame('', $response->getContent());
+    }
+
+    /**
+     * Test with content.
+     */
+    public function testWithContent()
+    {
+        $application = new BasicTestApplication(FilePath::parse('/var/www/'));
+        $request = new BasicTestRequest(Url::parse('https://www.example.com/foo/bar'), new Method('GET'));
+        $response = new BasicTestResponse();
+        $actionResultException = new NotFoundResultException('The page was not found.');
+        $actionResultException->getActionResult()->updateResponse($application, $request, $response);
+
+        self::assertSame(404, $response->getStatusCode()->getCode());
+        self::assertSame('Not Found', $response->getStatusCode()->getDescription());
+        self::assertSame('The page was not found.', $response->getContent());
+    }
+}
